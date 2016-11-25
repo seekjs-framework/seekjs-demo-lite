72c851c7d1a3263bba5eecc8cc000abf44ab203e
利开团 <likaituan@wecash.net>
2016-11-25 11:36:21 +0800

    quick submit

--- main.js
+++ main.js
@@ -2,26 +2,10 @@
  * Created by likaituan on 15/8/9.
  */
 
-seekjs.config({
-    ns:{
-        "js.": "/js/",
-        "css.": {
-            path: "/css/",
-            type: ".css"
-        }
-    }
-});
-
-require("css.tag");
-require("css.class");
-
 var app = require("sys.app");
 
-app.viewEx = require("js.view");
-app.pipeEx = require("js.pipe");
-
 app.config({
-    page: `/pages/`
+    page: `/`
 });
 
 app.init("home");
